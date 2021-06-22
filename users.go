4db9d5c2f5d7b8c28a6cba1c15b6c7d82a012380
maxsupermanhd <q3.max.2011@ya.ru>
2021-06-22 14:22:17 +0300

    Make users page actually function

--- users.go
+++ users.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -23,13 +24,38 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 			basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msgred": true, "msg": "Form parse error: " + err.Error()})
 			return
 		}
+		if r.PostFormValue("param") != "allow_preset_request" && r.PostFormValue("param") != "allow_host_request" && r.PostFormValue("param") != "norequest_reason" {
+			basicLayoutLookupRespond("error403", w, r, map[string]interface{}{})
+			return
+		}
+		if r.PostFormValue("param") == "allow_preset_request" || r.PostFormValue("param") == "allow_host_request" {
+			if r.PostFormValue("val") != "true" && r.PostFormValue("val") != "false" {
+				basicLayoutLookupRespond("error403", w, r, map[string]interface{}{})
+				return
+			}
+		}
+		if r.PostFormValue("name") == "" {
+			basicLayoutLookupRespond("error403", w, r, map[string]interface{}{})
+			return
+		}
+		tag, derr := dbpool.Exec(context.Background(), "UPDATE users SET "+r.PostFormValue("param")+" = $1 WHERE username = $2", r.PostFormValue("val"), r.PostFormValue("name"))
+		if derr != nil {
+			basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msgred": true, "msg": "Database query error: " + derr.Error()})
+			return
+		}
+		if tag.RowsAffected() != 1 {
+			basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msgred": true, "msg": "Rows affected " + strconv.Itoa(int(tag.RowsAffected()))})
+			return
+		}
+		basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msggreen": true, "msg": "Success"})
+		w.Header().Set("Refresh", "1; /users")
 	} else {
-		rows, derr := dbpool.Query(context.Background(), `select to_json(users) from users order by id desc;`)
+		rows, derr := dbpool.Query(context.Background(), `select to_json(users) from users order by id asc;`)
 		if derr != nil {
 			if derr == pgx.ErrNoRows {
 				basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msg": "No games played"})
 			} else {
-					basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msgred": true, "msg": "Database query error: " + derr.Error()})
+				basicLayoutLookupRespond("plainmsg", w, r, map[string]interface{}{"msgred": true, "msg": "Database query error: " + derr.Error()})
 			}
 			return
 		}
