5f4872527d1e7d205cfb8551bb8559d054ec66f7
maxsupermanhd <q3.max.2011@ya.ru>
2022-06-20 19:14:10 +0300

    Tweak undername text

--- main.go
+++ main.go
@@ -412,7 +412,11 @@ where hash = $1`, hash).Scan(&de, &de2, &dap, &daw, &dal, &dui, &dep, &drp, &dpi
 		m.Details += "\nRating lookup from\nhttps://wz2100-autohost.net/"
 		if elo == "" {
 			if dui != -1 && dui != 0 {
-				m.Elo = fmt.Sprintf("R[%d] E[%d] W%d/L%d", de2, de, daw, dal)
+				if dap > 0 {
+					m.Elo = fmt.Sprintf("R[%d] E[%d] %d %.1f%%", de2, de, dap, float64(daw)/float64(dap))
+				} else {
+					m.Elo = fmt.Sprintf("R[%d] E[%d] %d -", de2, de, dap)
+				}
 			} else {
 				m.Elo = fmt.Sprintf("unapproved E[%d] W%d/L%d", de, daw, dal)
 			}
